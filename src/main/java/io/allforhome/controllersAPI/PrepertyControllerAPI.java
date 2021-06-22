ae43a94c7ebed8ccfccd0b1d5dc35a9bfe042fc6
Mouloud Kemiche <k_mouloud@icloud.com>
2021-06-21 22:28:11 -0400

    * add swagger documentation API

--- src/main/java/io/allforhome/controllersAPI/PrepertyControllerAPI.java
+++ src/main/java/io/allforhome/controllersAPI/PrepertyControllerAPI.java
@@ -2,6 +2,8 @@ package io.allforhome.controllersAPI;
 
 import io.allforhome.models.Property;
 import io.allforhome.services.PropertyService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -14,17 +16,20 @@ import java.util.List;
  */
 
 @RestController
+@Api(value="API describe CRUD operation for property object")
 @RequestMapping("api/property")
 public class PrepertyControllerAPI {
 
     @Autowired
     PropertyService propertyService;
 
+    @ApiOperation(value = "Return all properties existing in the database")
     @GetMapping(value = "/getallproperties", produces = "application/json")
     public List<Property> getAllProperties(){
         return propertyService.getAllProperties();
     }
 
+    @ApiOperation(value = "Return property by its id if exists")
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Property getProperyById(@PathVariable("id") Long id){
         //return RestPreconditions.checkFound
@@ -32,16 +37,19 @@ public class PrepertyControllerAPI {
         return p;
     }
 
+    @ApiOperation(value = "Register new property")
     @PostMapping("/newproperty")
     public void newProperty(@RequestBody Property property){
         propertyService.createProperty(property);
     }
 
+    @ApiOperation(value = "Update property by its id if exists")
     @PutMapping("/{id}")
     public void updateProperty(@PathVariable("id") Long id, @RequestBody Property property){
         propertyService.updateProperty(property);
     }
 
+    @ApiOperation(value = "Remove property by its id if exists")
     @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public void deleteProperty(@PathVariable("id") Long id){
         propertyService.removeProperty(id);
--- src/main/java/io/allforhome/controllersAPI/UserControllerAPI.java
+++ src/main/java/io/allforhome/controllersAPI/UserControllerAPI.java
@@ -6,6 +6,8 @@ import io.allforhome.models.Agent;
 import io.allforhome.models.PrivateUser;
 import io.allforhome.models.User;
 import io.allforhome.services.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,17 +19,20 @@ import java.util.Optional;
  * @created 21/06/2021
  */
 @RestController
+@Api(value="API describe CRUD operation for user object")
 @RequestMapping("api/user")
 public class UserControllerAPI {
 
     @Autowired
     private UserService userService;
 
+    @ApiOperation(value = "Return all users existing in the database")
     @GetMapping(value = "/getallusers", produces = "application/json")
     public List<User> getAllUsers(){
         return userService.getAllUser();
     }
 
+    @ApiOperation(value = "Return user by its id if exists")
     @GetMapping(value = "/{id}")
     public User getUserById(@PathVariable("id") Long id){
         Optional<User> user = userService.getUserById(id);
@@ -37,6 +42,7 @@ public class UserControllerAPI {
         return user.get();
     }
 
+    @ApiOperation(value = "Register new user")
     @PostMapping("/newuser")
     public void newUser(@RequestBody PrivateUser user){
         user.setURoles(Role.ROLE_PRIVATE_USER.getRole());
@@ -50,11 +56,13 @@ public class UserControllerAPI {
         userService.saveUser(agent);
     }
 
+    @ApiOperation(value = "Update user by its id if exists")
     @PutMapping("/{id}")
     public void updateUser(@PathVariable("id") Long id, @RequestBody User user) throws Exception {
         userService.updateUser(user);
     }
 
+    @ApiOperation(value = "Remove property by its id if exists")
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable("id") Long id) throws Exception {
         userService.removeUser(id);
